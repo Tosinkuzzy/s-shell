f6c6032ca35466fec765f6e1d1eb8d353fdf9d97
Tosinkuzzy <adeleye608@gmail.com>
2023-10-14 01:32:49 +0100

    Print strings attached with write to the stdout

---
+++ print_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _printstring = Print string to stdout.
+*
+* Return: Always 0.
+*/
+int _printstring(char *str)
+{
+int i = 0, count = 0;
+while (str[i])
+{
+count += _putchar(str[i++]);
+}
+return(count);
+}
