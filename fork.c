f431419414d2552191acd102e4c76c5e2573bc2c
Tosinkuzzy <adeleye608@gmail.com>
2023-10-19 00:25:02 +0100

    fork updated

--- fork.c
+++ fork.c
@@ -3,6 +3,7 @@
 
 /**
 * main - fork
+* my_pid: getpid
 *
 * Return: Always 0.
 */
@@ -11,15 +12,18 @@ int main(void)
 pid_t my_pid;
 pid_t pid;
 
-printf("Before fork\n");
+/* printf("Before fork\n"); */
 pid = fork();
 if (pid == -1)
 {
 perror("Error:");
 return (1);
 }
-printf("After fork\n");
+/*  printf("After fork\n"); */
 my_pid = getpid();
-printf("My pid is %u\n", my_pid);
+if (my_pid == -1)
+{
+/* printf("My pid is %u\n", my_pid); */
 return (0);
+}
 }
