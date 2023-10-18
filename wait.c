ce52ea356d22d42d129e57b9cfe8e1d535e8e465
Tosinkuzzy <adeleye608@gmail.com>
2023-10-19 00:49:10 +0100

    wait status

---
+++ wait.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+* main - fork & wait
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+pid_t Child_pid;
+int status;
+
+Child_pid = fork();
+if (Child_pid == -1)
+{
+perror("Error");
+return (1);
+}
+if (Child_pid == 0)
+{
+print_string("wait for me, wait for me\n");
+sleep(3);
+}
